7d6a2edd0cd752bd3f7e783ddbe784980d09497a
zhamdi.into <zhamdi.into@f3df22b3-3b61-a262-660c-df172c72d05d>
2011-06-02 12:23:17 +0000

    trunk continuing (test enabled)

--- advanced-suggest-select-box/src/eu/nextstreet/gwt/components/client/IntoGwt.java
+++ advanced-suggest-select-box/src/eu/nextstreet/gwt/components/client/IntoGwt.java
@@ -29,7 +29,7 @@ public class IntoGwt implements EntryPoint {
 	/**
 	 * When set to true, the testing demo app is launched
 	 */
-	private boolean test = false;
+	private boolean test = true;
 
 	/**
 	 * This is the entry point method.
