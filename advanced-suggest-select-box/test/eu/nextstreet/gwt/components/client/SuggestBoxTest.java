ca7c9c543cfc0d4512312c81390f2c0a165dbd34
zhamdi.into@gmail.com <zhamdi.into@gmail.com@f3df22b3-3b61-a262-660c-df172c72d05d>
2012-04-04 12:14:21 +0000

    Asynchroneous treatement is ok

---
+++ advanced-suggest-select-box/test/eu/nextstreet/gwt/components/client/SuggestBoxTest.java
@@ -0,0 +1,12 @@
+package eu.nextstreet.gwt.components.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class SuggestBoxTest implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		ValidationTest.testWidget();
+	}
+
+}
