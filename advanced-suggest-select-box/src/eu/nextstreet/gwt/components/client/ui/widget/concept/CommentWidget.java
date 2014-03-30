630c9461f3f26003ccee59ad61c89086f77875bf
zHamdi@gmail.com <zHamdi@gmail.com@f3df22b3-3b61-a262-660c-df172c72d05d>
2014-03-30 12:53:46 +0000

    Added the CommentWidget class, and tested in demo

---
+++ advanced-suggest-select-box/src/eu/nextstreet/gwt/components/client/ui/widget/concept/CommentWidget.java
@@ -0,0 +1,53 @@
+package eu.nextstreet.gwt.components.client.ui.widget.concept;
+
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * switches between two different styled components depending on whether there is data or not inside (somehow as the g+ or fb comment)
+ * 
+ * @author zied
+ * 
+ */
+public class CommentWidget extends SwitchOnEditWidget<TextBox, TextArea, String> {
+
+	public CommentWidget(String data, String defaultText) {
+		this(data, defaultText, "emptyComment", "comment");
+	}
+
+	public CommentWidget(String data, String defaultText, String emptyStyle, String dataStyle) {
+		super(new TextBox(), new TextArea(), data);
+		emptyComp.setText(defaultText);
+		setEmptyStyle(emptyStyle);
+		setNonEmptyStyle(dataStyle);
+	}
+
+	public void setNonEmptyStyle(String dataStyle) {
+		dataComp.setStyleName(dataStyle);
+	}
+
+	public void setEmptyStyle(String emptyStyle) {
+		emptyComp.setStyleName(emptyStyle);
+	}
+
+	@Override
+	boolean isEmptyContent() {
+		String data = getData();
+		return data == null || data.trim().length() == 0;
+	}
+
+	@Override
+	void setData(String data) {
+		dataComp.setText(data);
+	}
+
+	@Override
+	String getData() {
+		return dataComp.getText();
+	}
+
+	@Override
+	protected void gainFocusOnDataComp() {
+		dataComp.setFocus(true);
+	}
+}
---
+++ advanced-suggest-select-box/src/eu/nextstreet/gwt/components/client/ui/widget/concept/SwitchOnEditWidget.java
@@ -0,0 +1,82 @@
+package eu.nextstreet.gwt.components.client.ui.widget.concept;
+
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * This class permit to have two components based on the {@link #isEmptyContent()} method. It also handles the dom events that trigger switching from a state to
+ * the other.
+ * 
+ * @author zied
+ * 
+ * @param <E>
+ *          the component displayed when the content is empty
+ * @param <D>
+ *          the component used when the item contains data, or while editing
+ * @param <T>
+ *          the data type
+ */
+public abstract class SwitchOnEditWidget<E extends Widget, D extends Widget, T> extends SimplePanel {
+	protected E emptyComp;
+	protected D dataComp;
+
+	protected final class DataCompHandler implements BlurHandler {
+		@Override
+		public void onBlur(BlurEvent event) {
+			if (isEmptyContent())
+				setWidget(emptyComp);
+		}
+	}
+
+	protected class EmptyCompHandler implements ClickHandler, FocusHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+			switchToDataComp();
+		}
+
+		@Override
+		public void onFocus(FocusEvent event) {
+			switchToDataComp();
+		}
+	}
+
+	public SwitchOnEditWidget(E emptyComp, D dataComp, T data) {
+		this.emptyComp = emptyComp;
+		this.dataComp = dataComp;
+		// init data before deciding on what to display
+		setData(data);
+		setWidget(isEmptyContent() ? emptyComp : dataComp);
+
+		initDomHandlers();
+	}
+
+	protected void initDomHandlers() {
+		EmptyCompHandler emptyCompHandler = new EmptyCompHandler();
+		emptyComp.addDomHandler(emptyCompHandler, ClickEvent.getType());
+		emptyComp.addDomHandler(emptyCompHandler, FocusEvent.getType());
+		dataComp.addDomHandler(new DataCompHandler(), BlurEvent.getType());
+	}
+
+	/**
+	 * this method is called when the user required the focus to edit, and the component switched to the editable one
+	 */
+	protected void gainFocusOnDataComp() {
+	}
+
+	abstract boolean isEmptyContent();
+
+	abstract void setData(T data);
+
+	abstract T getData();
+
+	public void switchToDataComp() {
+		setWidget(dataComp);
+		gainFocusOnDataComp();
+	}
+}
