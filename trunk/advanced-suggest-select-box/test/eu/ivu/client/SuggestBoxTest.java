a9d304c0ae5d128c5f47ff25db657cdd696c420d
zHamdi@gmail.com <zHamdi@gmail.com@f3df22b3-3b61-a262-660c-df172c72d05d>
2014-12-24 10:07:00 +0000

    forgot to disable debug mode

--- trunk/advanced-suggest-select-box/test/eu/ivu/client/SuggestBoxTest.java
+++ trunk/advanced-suggest-select-box/test/eu/ivu/client/SuggestBoxTest.java
@@ -2,13 +2,11 @@ package eu.ivu.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
-import eu.nextstreet.gwt.components.client.ui.widget.suggest.impl.DefaultSuggestionPopup;
-
 public class SuggestBoxTest implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		DefaultSuggestionPopup.DEBUG_MODE = true;
+		// DefaultSuggestionPopup.DEBUG_MODE = true;
 		ValidationTest.testWidget();
 	}
 
